b5bcb7e2e653379f8ee7c6cc795eb834fe2759ff
nileshg4444 <nileshcsevnit@gmail.com>
2015-02-26 23:43:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -86,6 +86,11 @@ app.put('/contactlist/:id', function(req,res){
 	
 	});
 
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+app.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 3000');
+});
 
-app.listen(3000);
-console.log("Server running on port no. 3000");
+//app.listen(3000);
+//console.log("Server running on port no. 3000");
