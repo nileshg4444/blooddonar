1675ef3a37fa8c22cf35ab4e1397f47a40cca79a
nileshg4444 <nileshcsevnit@gmail.com>
2015-02-27 01:20:01 +0530

    Update Server.js with data base credentials

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require ('express');
 var app= express();
 var mongojs = require('mongojs');
-var db= mongojs('contactlist',['contactlist']);
+var db= mongojs('admin:YZd534XXfzxR@mongodb://$OPENSHIFT_MONGODB_DB_HOST:$OPENSHIFT_MONGODB_DB_PORT/blooddonar',['contactlist']);
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname +"/public/"));
